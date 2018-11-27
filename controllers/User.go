4515dad16a3275a1f8a984e28fa81a8533378438
guoliang <guoliang@qq.com>
2018-11-28 01:04:10 +0800

    调试

--- controllers/User.go
+++ controllers/User.go
@@ -2,12 +2,15 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"my-note-api/models"
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	"strconv"
 )
 
 type UserController struct {
 	beego.Controller
 }
-
 
 //func init(this *UserController) {
 //	this.user = new models.UserModel
@@ -15,16 +18,25 @@ type UserController struct {
 
 func (this *UserController) Get() {
 
+	o := orm.NewOrm()
+	o.Using("default")
+	//用户列表
+	idval,errId:=strconv.ParseInt(this.GetString("Id"),10,64);
+	if errId!=nil{
+		fmt.Println("缺少参数id");
+	}
+	user,err:=models.QueryById(idval)
+	if err==true{
+		fmt.Println("获取模型失败");
+		fmt.Println(err);
+	}else{
+		fmt.Println("获取模型成功");
+	}
+
 	this.Data["json"] = map[string]interface{}{
 		"code" : 200,
+		"msg" : "success",
+		"data" : user,
 	}
 	this.ServeJSON()
-
-	//o := orm.NewOrm()
-	//
-	//user := models.UserModel{username: "slene"}
-	//
-	//// insert
-	//id, err := o.Insert(&user)
-	//fmt.Printf("ID: %d, ERR: %v\n", id, err)
 }
--- models/UserModel.go
+++ models/UserModel.go
@@ -1,58 +1,155 @@
 package models
 
 import (
-	"github.com/astaxie/beego/orm"
-	_ "github.com/go-sql-driver/mysql" // import your used driver
 	"fmt"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// Model Struct
-type UserModel struct {
-	id   int
-	username string `orm:"size(150)"`
-	password string `orm:"size(255)"`
-	status int
-	create_time int
-	last_time int
-
+// 用户
+type User struct{
+	Id              int64    `orm:"auto"`
+	Name            string   `orm:"size(100)"`
 }
 
-func init() {
-	// set default database
-	orm.RegisterDataBase(
-		"default",
-		"mysql",
-		"username:password@tcp(127.0.0.1:3306)/db_name?charset=utf8",
-		30,
-	)
+//新增用户
+func Create(id int64,name string)  (user User){
 
-	// register model
-	orm.RegisterModel(new(UserModel))
+	//查询用户是否已存在
+	user, err := QueryById(id)
+	if err == true{
+		return user
+	}else{
+		o := orm.NewOrm()
+		o.Using("default")
+		newuser:=new(User);
+		//赋值给模型
+		newuser.Id = id
+		newuser.Name = name
 
-	// create table
-	//orm.RunSyncdb("default", false, true)
+		//新增数据
+		o.Insert(newuser)
+
+		return *newuser
+	}
+}
+//删除用户
+func DeleteById(id int64) bool {
+
+	o := orm.NewOrm()
+	o.Using("default")
+	//根据ID得到用户模型
+	if num, err := o.Delete(&User{Id: id}); err == nil {
+		fmt.Println("删除影响的行数:")
+		fmt.Println(num)
+		return true
+	}else{
+		return false
+	}
 }
 
-func main() {
+//更新用户
+func UpdateById(id int,table string,filed map[string] interface{})bool{
+	o := orm.NewOrm()
+	_, err := o.QueryTable(table).Filter("id", id).Update(filed)
+	if err == nil{
+		return true
+	}
+	return false
+}
+
+
+//根据用户ID查询用户
+func QueryById(id int64) (User, bool){
+
+	o := orm.NewOrm()
+	u := User{Id: id}
+
+	err := o.Read(&u)
+
+	if err == orm.ErrNoRows {
+		fmt.Println("查询不到")
+		return u,false
+	} else if err == orm.ErrMissPK {
+		fmt.Println("找不到主键")
+		return u,false
+	} else {
+		fmt.Println(u.Id, u.Name)
+		return u,true
+	}
+}
+
+//根据用户名称查询用户
+func QueryByName(name string) (User, error) {
+	var user User
+
+	o := orm.NewOrm()
+	qs := o.QueryTable("user")
+
+	err := qs.Filter("name", name).One(&user)
+	fmt.Println(err)
+	if err == nil {
+		fmt.Println(user.Name)
+		return user,nil
+	}
+	return user, err
+}
+
+//根据用户数据列表
+func DataList() (users []User) {
+
+	o := orm.NewOrm()
+	qs := o.QueryTable("user")
+
+	var us []User
+	cnt, err :=  qs.Filter("id__gt", 0).OrderBy("-id").Limit(10, 0).All(&us)
+	if err == nil {
+		fmt.Printf("count", cnt)
+	}
+	return us
+}
+
+//查询语句，sql语句的执行
+//格式类似于:o.Raw("UPDATE user SET name = ? WHERE name = ?", "testing", "slene")
+//
+func QueryBySql(sql string, qarms[] string) bool{
+
 	o := orm.NewOrm()
 
-	user := UserModel{username: "slene"}
+	//执行sql语句
+	o.Raw(sql, qarms)
 
-	// insert
-	id, err := o.Insert(&user)
-	fmt.Printf("ID: %d, ERR: %v\n", id, err)
+	return true
+}
+//根据用户分页数据列表
+func LimitList(pagesize int,pageno int) (users []User) {
 
-	// update
-	user.username = "astaxie"
-	num, err := o.Update(&user)
-	fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+	o := orm.NewOrm()
+	qs := o.QueryTable("user")
 
-	// read one
-	u := UserModel{id: user.id}
-	err = o.Read(&u)
-	fmt.Printf("ERR: %v\n", err)
+	var us []User
+	cnt, err :=  qs.Limit(pagesize, (pageno-1)*pagesize).All(&us)
+	if err == nil {
+		fmt.Printf("count", cnt)
+	}
+	return us
+}
+//根据用户数据总个数
+func GetDataNum() int64 {
 
-	// delete
-	num, err = o.Delete(&u)
-	fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+	o := orm.NewOrm()
+	qs := o.QueryTable("user")
+
+	var us []User
+	num, err :=  qs.Filter("id__gt", 0).All(&us)
+	if err == nil {
+		return num
+	}else{
+		return 0
+	}
+}
+//初始化模型
+func init() {
+	// 需要在init中注册定义的model
+	orm.RegisterModel(new(User))
 }
